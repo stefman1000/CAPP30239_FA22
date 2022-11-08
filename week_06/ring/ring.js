3a5fb90d201b044cb4141b4dc73c8b8175269864
stefman1000 <Stefan@MacBook-Pro-4.local>
2022-11-08 14:02:41 -0600

    added week_06 homework

--- week_06/ring/ring.js
+++ week_06/ring/ring.js
@@ -1,13 +1,14 @@
 d3.json('budget-2022.json').then((data) => {
   const height = 400,
     width = 600,
-    innerRadius = 125,
+    innerRadius = 100,
     outerRadius = 175,
     labelRadius = 200;
 
   const arcs = d3.pie().value(d => d.amount)(data);
   const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius);
   const arcLabel = d3.arc().innerRadius(labelRadius).outerRadius(labelRadius);
+  console.log(arcs);
 
   const svg = d3.select("#chart")
     .append("svg")
@@ -16,38 +17,38 @@ d3.json('budget-2022.json').then((data) => {
     .attr("viewBox", [-width / 2, -height / 2, width, height])
     .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
 
-  svg.append("g")
+  svg.append("g")     // makes white seperators between arcs
     .attr("stroke", "white")
     .attr("stroke-width", 2)
     .attr("stroke-linejoin", "round")
-    .selectAll("path")
+    .selectAll("path")        // begin data join, put data into buckets
     .data(arcs)
     .join("path")
     .attr("fill", (d, i) => d3.schemeCategory10[i])
     .attr("d", arc);
 
-  // svg.append("g")
-  //   .attr("font-size", 10)
-  //   .attr("text-anchor", "middle")
-  //   .selectAll("text")
-  //   .data(arcs)
-  //   .join("text")
-  //   .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
-  //   .selectAll("tspan")
-  //   .data(d => {
-  //     return [d.data.category, d.data.amount];
-  //   })
-  //   .join("tspan")
-  //   .attr("x", 0)
-  //   .attr("y", (d, i) => `${i * 1.1}em`)
-  //   .attr("font-weight", (d, i) => i ? null : "bold")
-  //   .text(d => d);
+  svg.append("g")
+    .attr("font-size", 10)
+    .attr("text-anchor", "middle")
+    .selectAll("text")
+    .data(arcs)
+    .join("text")
+    .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
+    .selectAll("tspan")     // creates a break in the line
+    .data(d => {
+      return [d.data.category, d.data.amount];
+    })
+    .join("tspan")
+    .attr("x", 0)
+    .attr("y", (d, i) => `${i * 1.1}em`)
+    .attr("font-weight", (d, i) => i ? null : "bold")
+    .text(d => d);
 
-  // svg.append("text")
-  //   .attr("font-size", 30)
-  //   .attr("font-weight", "bold")
-  //   .attr("text-anchor", "middle")
-  //   .attr("alignment-baseline", "middle")
-  //   .text("2022")
-  //   .style("font-size", 20);
+  svg.append("text")
+    .attr("font-size", 30)
+    .attr("font-weight", "bold")
+    .attr("text-anchor", "middle")
+    .attr("alignment-baseline", "middle")
+    .text("2022")
+    .style("font-size", 20);
 });

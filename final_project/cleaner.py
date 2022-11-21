b0094f621effdabcafbf4e9e97e0c4c192240541
stefman1000 <Stefan@MacBook-Pro-4.local>
2022-11-21 01:27:16 -0500

    some fixes

--- final_project/cleaner.py
+++ final_project/cleaner.py
@@ -3,13 +3,10 @@ import numpy as np
 import json
 
 
-df =  pd.read_csv("chicago_crime.csv")
-
-<<<<<<< HEAD
+df =  pd.read_csv("final_project/chicago_crime.csv")
 
 
-to_drop = ['Case Number','Wards', 'Police Beats','ID','FBI Code', 'X Coordinate', 'Y Coordinate', 'Beat','District', 'IUCR', "Police Districts", "Boundaries - ZIP Codes" ]
-df.drop(to_drop, inplace=True, axis=1)
+
 grouped = df.groupby("Year")
 
 grouped = df.groupby("Community Area", axis="columns")
@@ -20,16 +17,15 @@ grouped = df.groupby(["Year", "Community Area"])
 counts = grouped.size()
 
 
-out = {'Community': grouped.size().to_dict()}
+out = grouped.size().to_dict()
 print(out)
 
 
-df.to_csv(r'final_project/chicago_crime.csv')
 
-with open("final_project/chicago_crime.json", "w") as outfile:
-    json.dump(out, outfile)
-## out.to_json(r"final_project/chicago_crime.json")
 
-=======
-print(df.head())
->>>>>>> parent of af22140... removed large file
+grouped2 = df.groupby('Year')[['Community Area','']].apply(lambda x: x.set_index('Chain').to_dict(orient='index')).to_dict()
+
+## with open("final_project/chicago_crime.json", "w") as outfile:
+    ## json.dump(out, outfile)
+counts.to_json(r"final_project/chicago_crime.json", orient = 'index')
+
